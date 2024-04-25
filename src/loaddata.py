638ba8b11e2182715b438875c5025e488a8e95e1
dynamicsamic <mirabyansr@gmail.com>
2024-04-25 23:12:33 +0300

    refactor: reduce verbosity in `load_modeldata` function

--- src/loaddata.py
+++ src/loaddata.py
@@ -1,61 +1,45 @@
 import csv
 import logging
 
-from advanced_alchemy.base import UUIDBase
 from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
-from .models import CityService
+from .models import Base
+from .repository import CityService, CustomerService
 from .settings import BASE_DIR, DATA_DIR
 from .utils import (
     check_tables_created,
     create_tables,
-    get_model,
     log_handler,
 )
 
 logger = logging.getLogger(__name__)
 logger.addHandler(log_handler)
 
+SERVICE_TO_SOURCE = {CityService: "City.csv", CustomerService: "Customer.csv"}
+
 
 async def load_modeldata(
     file_name: str,
-    model_name: str,
-    async_session: AsyncSession,
     service: SQLAlchemyAsyncRepositoryService,
+    async_session: AsyncSession,
 ) -> None:
     """Populate single database table from a source file.
 
     Args:
         `file_name`: exact name of a source file.
-        `model_name`: exact name of your model.
-        `async_session`: instance of sqlalchemy `AsyncSession`.
         `service`: instance of advanced_alchemy
             `SQLAlchemyAsyncRepositoryService`
-    """
-    with open(BASE_DIR / DATA_DIR / file_name) as file:
-        reader = csv.DictReader(file)
-        model = get_model(model_name)
-        inserted = await service(async_session).create_many(
-            [model(**item) for item in reader]
-        )
-        logger.info(
-            f"Inserted {len(inserted)} items for model `{model_name}`."
-        )
-
-
-async def load_city(async_session: AsyncSession) -> None:
-    """Populate `City` table.
-
-    Args:
         `async_session`: instance of sqlalchemy `AsyncSession`.
     """
-    await load_modeldata(
-        async_session=async_session,
-        file_name="City.csv",
-        model_name="City",
-        service=CityService,
-    )
+    model = service.repository_type.model_type
+    with open(BASE_DIR / DATA_DIR / file_name) as file:
+        reader = csv.DictReader(file)
+        inserted = await service(async_session).create_many(reader)
+        logger.info(
+            f"Inserted {len(inserted)} items for model "
+            f"`{model.__tablename__.capitalize()}`."
+        )
 
 
 async def load_data(
@@ -70,5 +54,8 @@ async def load_data(
     """
     async with async_engine.begin() as conn:
         if not await check_tables_created(conn):
-            await create_tables(conn, UUIDBase)
-        await load_city(async_session)
+            await create_tables(conn, Base)
+        for service, file in SERVICE_TO_SOURCE.items():
+            await load_modeldata(
+                file_name=file, service=service, async_session=async_session
+            )
