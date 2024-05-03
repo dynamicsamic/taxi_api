ec270662190cc31666af9b38bdd43c5223e7dffe
dynamicsamic <mirabyansr@gmail.com>
2024-05-03 23:46:41 +0300

    feat: add new models; update old ones

--- src/models.py
+++ src/models.py
@@ -1,7 +1,15 @@
+import datetime as dt
 import enum
 
 from advanced_alchemy.base import UUIDBase
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import (
+    Date,
+    Enum,
+    ForeignKey,
+    Integer,
+    String,
+    UniqueConstraint,
+)
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -10,6 +18,8 @@ Base = UUIDBase
 
 class AbstractBase(Base):
     __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
     def _fmt(self, description: str) -> str:
         """Return nice-looking model instance description.
@@ -33,12 +43,27 @@ class PaymentMethod(enum.Enum):
     Cash = "Cash"
 
 
+class Carrier(AbstractBase):
+    __tablename__ = "carriers"
+
+    name: Mapped[str] = mapped_column(String, unique=True)
+    trips: Mapped[list["Trip"]] = relationship(
+        back_populates="carrier", cascade="save-update, merge", lazy="noload"
+    )
+
+    def __str__(self) -> str:
+        return self._fmt(self.name)
+
+
 class City(AbstractBase):
     __tablename__ = "cities"
 
     name: Mapped[str]
     population: Mapped[int]
     users: Mapped[int]
+    trips: Mapped[list["Trip"]] = relationship(
+        back_populates="city", cascade="save-update, merge", lazy="noload"
+    )
 
     def __str__(self) -> str:
         return self._fmt(
@@ -58,28 +83,59 @@ class Customer(AbstractBase):
         back_populates="customer",
         cascade="delete, merge, save-update",
         passive_deletes=True,
+        lazy="noload",
     )
 
     def __str__(self) -> str:
         return self._fmt(
-            f"{self.gender}, {self.age} years, ${self.month_income}"
+            f"{self.gender.value}, {self.age} years, ${self.month_income}"
         )
 
 
 class Transaction(AbstractBase):
     __tablename__ = "transactions"
 
-    customer_id: Mapped[int] = mapped_column(
-        ForeignKey("customers.id", ondelete="CASCADE")
-    )
-    customer: Mapped[Customer] = relationship(back_populates="transactions")
     payment_method: Mapped[Enum] = mapped_column(
         Enum(PaymentMethod, create_constraint=True, validate_strings=True)
     )
+    customer: Mapped[Customer] = relationship(back_populates="transactions")
+    customer_id: Mapped[int] = mapped_column(
+        ForeignKey("customers.id", ondelete="CASCADE")
+    )
+    trip: Mapped["Trip"] = relationship(back_populates="transaction")
 
     def __str__(self) -> str:
         return self._fmt(
-            f"CustomerId {self.customer_id}, {self.payment_method}"
+            f"CustomerId {self.customer_id}, {self.payment_method.value}"
+        )
+
+
+class Trip(AbstractBase):
+    __tablename__ = "trips"
+
+    date: Mapped[dt.date] = mapped_column(Date, default=dt.date.today())
+    distance: Mapped[int]
+    price: Mapped[int]
+    cost: Mapped[int]
+    transaction: Mapped[Transaction] = relationship(back_populates="trip")
+    transaction_id: Mapped[int] = mapped_column(
+        ForeignKey("transactions.id", ondelete="RESTRICT")
+    )
+    carrier: Mapped[Carrier] = relationship(back_populates="trips")
+    carrier_id: Mapped[int] = mapped_column(
+        ForeignKey("carriers.id", ondelete="RESTRICT")
+    )
+    city: Mapped[City] = relationship(back_populates="trips")
+    city_id: Mapped[int] = mapped_column(
+        ForeignKey("cities.id", ondelete="RESTRICT")
+    )
+
+    __table_args__ = (UniqueConstraint("transaction_id"),)
+
+    def __str__(self) -> str:
+        return self._fmt(
+            f"{self.date}, {self.distance} km, $ {self.price}, "
+            f"CarrierId {self.carrier_id}, CityId {self.city_id}"
         )
 
 
