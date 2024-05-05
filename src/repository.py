5d97b7b1dacd13ad047ea72645314bb60650e832
dynamicsamic <mirabyansr@gmail.com>
2024-05-05 22:47:15 +0300

    feat: add repositories and services for all db models

--- src/repository.py
+++ src/repository.py
@@ -1,7 +1,11 @@
 from advanced_alchemy.repository import SQLAlchemyAsyncRepository
 from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService
 
-from .models import City, Customer
+from .models import Carrier, City, Customer, Transaction, Trip
+
+
+class CarrierRepository(SQLAlchemyAsyncRepository[Carrier]):
+    model_type = Carrier
 
 
 class CityRepository(SQLAlchemyAsyncRepository[City]):
@@ -12,9 +16,29 @@ class CustomerRepository(SQLAlchemyAsyncRepository[Customer]):
     model_type = Customer
 
 
+class TransactionRepository(SQLAlchemyAsyncRepository[Transaction]):
+    model_type = Transaction
+
+
+class TripRepository(SQLAlchemyAsyncRepository[Trip]):
+    model_type = Trip
+
+
+class CarrierService(SQLAlchemyAsyncRepositoryService[Carrier]):
+    repository_type = CarrierRepository
+
+
 class CityService(SQLAlchemyAsyncRepositoryService[City]):
     repository_type = CityRepository
 
 
 class CustomerService(SQLAlchemyAsyncRepositoryService[Customer]):
     repository_type = CustomerRepository
+
+
+class TransactionService(SQLAlchemyAsyncRepositoryService[Transaction]):
+    repository_type = TransactionRepository
+
+
+class TripService(SQLAlchemyAsyncRepositoryService[Trip]):
+    repository_type = TripRepository
