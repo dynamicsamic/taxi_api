1b59ac14ac9f995ffcbaf6bbf4c2c6d5fe1094de
dynamicsamic <mirabyansr@gmail.com>
2024-05-16 23:00:02 +0300

    tests: update create test data fixtures; add new basic tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,118 @@
+import random
+
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from src.loaddata import load_data
-from src.models import Base
-from src.repository import CityService
-from src.utils import db_session, drop_tables
+from src.models import Base, Gender, PaymentMethod
+from src.repository import (
+    CarrierService,
+    CityService,
+    CustomerService,
+    TransactionService,
+    TripService,
+)
+from src.utils import (
+    create_random_date,
+    create_tables,
+    db_session,
+    drop_tables,
+)
 
 test_engine = create_async_engine("sqlite+aiosqlite://", echo=True)
 async_session = async_sessionmaker(test_engine, expire_on_commit=False)
 
+CARRIER_SIZE = 10
+CITY_SIZE = 50
+CUSTOMER_SIZE = 10000
+TRANSACTION_SIZE = 100
+TRIP_SIZE = TRANSACTION_SIZE
 
-@pytest_asyncio.fixture
-async def test_session():
+
+async def create_carriers(session):
+    await CarrierService(session).create_many(
+        [{"id": i, "name": f"carrier{i}"} for i in range(1, CARRIER_SIZE + 1)]
+    )
+
+
+async def create_cities(session):
+    await CityService(session).create_many(
+        [
+            {
+                "id": i,
+                "name": f"city{i}",
+                "population": random.randrange(1000, 2000000),
+                "users": random.randrange(10, 100000),
+            }
+            for i in range(1, CITY_SIZE + 1)
+        ]
+    )
+
+
+async def create_customers(session):
+    await CustomerService(session).create_many(
+        [
+            {
+                "id": i,
+                "gender": random.choice((Gender.Female, Gender.Male)),
+                "age": random.randrange(18, 100),
+                "month_income": random.randrange(10000, 300000),
+            }
+            for i in range(1, CUSTOMER_SIZE + 1)
+        ]
+    )
+
+
+async def create_transactions(session):
+    await TransactionService(session).create_many(
+        [
+            {
+                "id": i,
+                "payment_method": random.choice(
+                    (PaymentMethod.Card, PaymentMethod.Cash)
+                ),
+                "customer_id": random.randrange(1, CUSTOMER_SIZE),
+            }
+            for i in range(1, TRANSACTION_SIZE + 1)
+        ]
+    )
+
+
+async def create_trips(session):
+    await TripService(session).create_many(
+        [
+            {
+                "id": i,
+                "date": create_random_date(),
+                "distance": random.randrange(100, 10000),
+                "price": random.randrange(1000, 100000),
+                "cost": random.randrange(1000, 100000),
+                "transaction_id": i,
+                "carrier_id": random.randrange(1, CARRIER_SIZE + 1),
+                "city_id": random.randrange(1, CITY_SIZE + 1),
+            }
+            for i in range(1, TRIP_SIZE + 1)
+        ]
+    )
+
+
+async def create_test_data(session):
+    create_functions = (
+        create_carriers,
+        create_cities,
+        create_customers,
+        create_transactions,
+        create_trips,
+    )
+    for fun in create_functions:
+        await fun(session)
+
+
+@pytest_asyncio.fixture(scope="module")
+async def create_test_tables():
+    async with test_engine.begin() as conn:
+        await create_tables(conn, Base)
     async with db_session(async_session) as session:
-        await load_data(test_engine, session)
+        await create_test_data(session)
         yield session
     async with test_engine.begin() as conn:
         await drop_tables(conn, Base)
-
-
-@pytest_asyncio.fixture
-async def cityservice(test_session):
-    return CityService(test_session)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,59 @@
 import pytest
+import pytest_asyncio
 
-from src.repository import CityService, CustomerService
+from src.repository import CarrierService, CityService, TripService
 
 pytestmark = pytest.mark.asyncio
 
-CITY_COUNT = 20
-CUSTOMER_COUNT = 49171
+
+CITY_MODELFIELDS = {"name": str, "population": int, "users": int, "id": int}
+CARIER_MODELFIELDS = {"name": str, "id": int}
 
 
-async def test_models(test_session):
-    cities = CityService(test_session)
-    customers = CustomerService(test_session)
-    assert await cities.count() == CITY_COUNT
-    assert await customers.count() == CUSTOMER_COUNT
+@pytest_asyncio.fixture(scope="module")
+async def cityservice(create_test_tables):
+    return CityService(create_test_tables)
+
+
+@pytest_asyncio.fixture(scope="module")
+async def carierservice(create_test_tables):
+    return CarrierService(create_test_tables)
+
+
+@pytest_asyncio.fixture(scope="module")
+async def tripservice(create_test_tables):
+    return TripService(create_test_tables)
+
+
+async def test_carier_modelfields(carierservice):
+    carier = await carierservice.get_one_or_none(id=1)
+    carier_dict = carier.to_dict()
+
+    assert all(
+        [
+            type(carier_dict[fieldname]) is fieldtype
+            for fieldname, fieldtype in CARIER_MODELFIELDS.items()
+        ]
+    )
+
+
+async def test_carier_noloads_trips(carierservice):
+    carier = await carierservice.get_one_or_none(id=1)
+    assert carier.trips == []
+
+
+async def test_city_modelfields(cityservice):
+    city = await cityservice.get_one_or_none(id=1)
+    city_dict = city.to_dict()
+
+    assert all(
+        [
+            type(city_dict[fieldname]) is fieldtype
+            for fieldname, fieldtype in CITY_MODELFIELDS.items()
+        ]
+    )
+
+
+async def test_city_noloads_trips(cityservice):
+    city = await cityservice.get_one_or_none(id=1)
+    assert city.trips == []
